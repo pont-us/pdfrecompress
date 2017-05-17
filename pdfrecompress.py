934f26bb0689915969af29f34192e3e315bc6de8
Pont Lurcock <pont@talvi.net>
2017-05-17 23:04:30 +0200

    Add brightness control; ignore small images.

--- pdfrecompress.py
+++ pdfrecompress.py
@@ -18,22 +18,30 @@ from tempfile import TemporaryDirectory
 def split_into_images(input_file, tempdir):
     prefix = os.path.join(tempdir, "a")
     pdfimages = Popen(
-        ["pdfimages", input_file, prefix]
+        ["pdfimages", "-j", input_file, prefix]
+        # "-j" write jpegs as jpegs
     )
     pdfimages.wait()
 
-def convert_to_bilevel_tiffs(tempdir):
+def convert_to_bilevel_tiffs(tempdir, brightness):
     for input_file in os.listdir(tempdir):
-        if input_file[-4:] not in (".ppm", ".pbm"):
+        if input_file[-4:] not in (".ppm", ".pbm", ".jpg"):
             continue
-        args = ["econvert", "-i", input_file, "--colorspace", "bilevel",
+        if os.stat(os.path.join(tempdir, input_file)).st_size < 1e5:
+            # Some files have small, extraneous images which
+            # we need to ignore.
+            # Under 100 kB means it's unlikely to be the
+            # main image from the page.
+            continue
+        args = ["econvert", "-i", input_file,
+                "--brightness", brightness,
+                 "--colorspace", "bilevel",
                 "-o", input_file[:-4]+".tiff"]
         econvert = Popen(args, cwd = tempdir)
         econvert.wait()
 
 def tiffs_to_pdf(tempdir, output_file):
     tiffs = sorted(glob.glob(os.path.join(tempdir, "*.tiff")))
-    print(tiffs)
     combined_tiff = os.path.join(tempdir, "all.tiff")
     args = ["tiffcp"] + tiffs + [combined_tiff]
     tiffcp = Popen(args)
@@ -56,13 +64,17 @@ def main():
                         type=str, nargs=1,
                         help="output filename")
 
+    parser.add_argument("--brightness", "-b", type=str, nargs=1,
+                        help="change image brightness",
+                        default="0")
+
     args = parser.parse_args()
 
     with TemporaryDirectory() as tempdir:
 
         split_into_images(args.input_file[0], tempdir)
 
-        convert_to_bilevel_tiffs(tempdir)
+        convert_to_bilevel_tiffs(tempdir, args.brightness[0])
 
         tiffs_to_pdf(tempdir, args.output_file[0])
 
